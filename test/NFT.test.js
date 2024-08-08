7734da9ef36d9f941fe8854fbc7b347ef8f05979
rajat-bakale <105482719+rajat-bakale@users.noreply.github.com>
2024-08-08 22:24:00 +0530

    Update NFT.test.js

--- test/NFT.test.js
+++ test/NFT.test.js
@@ -2,34 +2,39 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("NFT Contract", function () {
-  let NFT, nft, owner, addr1;
+  let nft;
+  let owner;
+  let addr1;
 
   beforeEach(async function () {
-    NFT = await ethers.getContractFactory("NFT");
     [owner, addr1] = await ethers.getSigners();
+    const NFT = await ethers.getContractFactory("NFT");
     nft = await NFT.deploy();
-    await nft.waitForDeployment();
+    await nft.deployed();
   });
 
   it("Should mint a new NFT", async function () {
-    const mintTx = await nft.mintNFT(addr1.address, "https://example.com/token/1");
+    const mintTx = await nft.mintNFT(addr1.address, "tokenURI_1");
     await mintTx.wait();
 
-    const ownerOfToken = await nft.ownerOf(1);
-    expect(ownerOfToken).to.equal(addr1.address);
-
-    const tokenURI = await nft.tokenURI(1);
-    expect(tokenURI).to.equal("https://example.com/token/1");
-  });
-
-  it("Should fail to mint NFT if not owner", async function () {
-    await expect(
-      nft.connect(addr1).mintNFT(addr1.address, "https://example.com/token/2")
-    ).to.be.revertedWith("Ownable: caller is not the owner");
-  });
-
-  it("Should return the correct total supply", async function () {
-    await nft.mintNFT(addr1.address, "https://example.com/token/1");
+    expect(await nft.ownerOf(1)).to.equal(addr1.address);
+    expect(await nft.tokenURI(1)).to.equal("tokenURI_1");
     expect(await nft.totalSupply()).to.equal(1);
+  });
+
+  it("Should return the total supply", async function () {
+    expect(await nft.totalSupply()).to.equal(0);
+
+    await nft.mintNFT(addr1.address, "tokenURI_1");
+    expect(await nft.totalSupply()).to.equal(1);
+
+    await nft.mintNFT(addr1.address, "tokenURI_2");
+    expect(await nft.totalSupply()).to.equal(2);
+  });
+
+  it("Should not allow non-owner to mint", async function () {
+    await expect(
+      nft.connect(addr1).mintNFT(addr1.address, "tokenURI_3")
+    ).to.be.revertedWith("Ownable: caller is not the owner");
   });
 });
