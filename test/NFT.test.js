72843c59db26c33553826fdfeb9a2be845fc5abc
rajat-bakale <105482719+rajat-bakale@users.noreply.github.com>
2024-08-08 23:00:28 +0530

    Update NFT.test.js

--- test/NFT.test.js
+++ test/NFT.test.js
@@ -2,39 +2,33 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("NFT Contract", function () {
+  let NFT;
   let nft;
   let owner;
   let addr1;
 
   beforeEach(async function () {
+    NFT = await ethers.getContractFactory("NFT");
     [owner, addr1] = await ethers.getSigners();
-    const NFT = await ethers.getContractFactory("NFT");
     nft = await NFT.deploy();
-    await nft.deployed();
+    await nft.waitForDeployment();
   });
 
-  it("Should mint a new NFT", async function () {
-    const mintTx = await nft.mintNFT(addr1.address, "tokenURI_1");
-    await mintTx.wait();
+  it("Should deploy the NFT contract and set the correct owner", async function () {
+    expect(await nft.owner()).to.equal(owner.address);
+  });
 
+  it("Should mint an NFT", async function () {
+    const tokenURI = "https://example.com/token1";
+    await nft.mintNFT(addr1.address, tokenURI);
+    expect(await nft.totalSupply()).to.equal(BigInt(1));
     expect(await nft.ownerOf(1)).to.equal(addr1.address);
-    expect(await nft.tokenURI(1)).to.equal("tokenURI_1");
-    expect(await nft.totalSupply()).to.equal(1);
+    expect(await nft.tokenURI(1)).to.equal(tokenURI);
   });
 
   it("Should return the total supply", async function () {
-    expect(await nft.totalSupply()).to.equal(0);
-
-    await nft.mintNFT(addr1.address, "tokenURI_1");
-    expect(await nft.totalSupply()).to.equal(1);
-
-    await nft.mintNFT(addr1.address, "tokenURI_2");
-    expect(await nft.totalSupply()).to.equal(2);
-  });
-
-  it("Should not allow non-owner to mint", async function () {
-    await expect(
-      nft.connect(addr1).mintNFT(addr1.address, "tokenURI_3")
-    ).to.be.revertedWith("Ownable: caller is not the owner");
+    const tokenURI = "https://example.com/token1";
+    await nft.mintNFT(addr1.address, tokenURI);
+    expect(await nft.totalSupply()).to.equal(BigInt(1));
   });
 });
