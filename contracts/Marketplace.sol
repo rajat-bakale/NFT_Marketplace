758096a3b4cac25ffa278b4487abd0c5030c44d6
rajat-bakale <105482719+rajat-bakale@users.noreply.github.com>
2024-08-08 22:22:34 +0530

    Update Marketplace.sol

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AlternativeNFTMarketplace is Ownable {
+contract NFTMarketplace is Ownable {
     struct Listing {
         uint256 tokenId;
         uint256 price;
